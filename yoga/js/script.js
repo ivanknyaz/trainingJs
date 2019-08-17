e82fd6b2b016e5278aa0fddc421ed4fdd2d693ec
Ivan <molodoi26@mail.ru>
2019-08-17 17:54:25 +0300

    ADD TIMER

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -34,4 +34,46 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //Timer
+
+    let deadLine = '2019-08-18';
+
+    function gerTimeRemaining(endTime){
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+        seconds = Math.floor((t)/1000 % 60),
+        minutes = Math.floor((t)/1000/60 % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            function updateClock() {
+                let t = gerTimeRemaining(endTime);
+                hours.textContent = t.hours < 10 ? '0' + t.hours : t.hours;
+                minutes.textContent = t.hours < 10 ? '0' + t.minutes : t.minutes;
+                seconds.textContent = t.seconds < 10 ? '0' + t.seconds : t.seconds;
+
+                if(t.total < 0) {
+                    clearInterval(timeInterval);
+                    hours.textContent = '00';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
+                }
+            }
+    }
+
+    setClock('timer', deadLine);
 });
